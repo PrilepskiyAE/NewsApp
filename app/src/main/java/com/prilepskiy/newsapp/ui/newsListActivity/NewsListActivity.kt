b7d2f6a3fd8fcc6a144f7c625b9841fbf5ebe44c
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-02-15 19:44:42 +0700

    test composable webview

--- app/src/main/java/com/prilepskiy/newsapp/ui/newsListActivity/NewsListActivity.kt
+++ app/src/main/java/com/prilepskiy/newsapp/ui/newsListActivity/NewsListActivity.kt
@@ -1,18 +1,23 @@
 package com.prilepskiy.newsapp.ui.newsListActivity
 
+
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.lifecycleScope
+import com.prilepskiy.newsapp.ui.newsListActivity.composable.WebViewContent
 import com.prilepskiy.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -21,7 +26,8 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class NewsListActivity : ComponentActivity() {
     val viewModel: NewsListViewModel by viewModels()
-
+   lateinit var url:String
+    lateinit var title:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,34 +35,33 @@ class NewsListActivity : ComponentActivity() {
         lifecycleScope.launchWhenStarted {
             viewModel.newsList.collectLatest {
                 Log.d("TAG", "onCreate:${it?.get(1)} ")
-            }
-        }
-
-        setContent {
-            NewsAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                if (it != null) {
+                    url = it.get(0).url.toString()
+                    title=it.get(0).title.toString()
+                    setContent {
+                        NewsContent(url, title = title)
+                    }
                 }
             }
         }
+
+    }
+    @Composable
+
+    fun NewsContent(url: String,title:String) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("News | $title") },
+                    backgroundColor = Color(0xff0f9d58)
+                )
+            },
+            content = {
+                it
+                WebViewContent(url)
+            }
+        )
     }
 
-
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NewsAppTheme {
-        Greeting("Android")
-    }
-}
