230c1fe3fbe24442cc0ecfa349a12883d73b64b3
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-02-17 01:35:21 +0700

    add topnews

--- app/src/main/java/com/prilepskiy/newsapp/ui/newsListActivity/composable/WebViewComposable.kt
+++ app/src/main/java/com/prilepskiy/newsapp/ui/newsListActivity/composable/WebViewComposable.kt
@@ -4,12 +4,14 @@ import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 @Composable
@@ -18,7 +20,8 @@ fun NewsContent(url: String="https://ura.news/news/1052626832",title:String="") 
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("News | $title", maxLines = 1 ) },
+                title = { Text("News | $title", maxLines = 1, style = MaterialTheme.typography.body1,
+                    overflow = TextOverflow.Ellipsis ) },
                 backgroundColor = Color(0xff0f9d58),
             )
         },
